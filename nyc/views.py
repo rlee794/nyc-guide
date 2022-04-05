220cf0d99f78e7b9b1f7604c1f8faf6d087ad7ef
rlee794 <rlee97@gmail.com>
2022-04-05 18:36:00 -0500

    updated venue for buttons and forms to return to homepage

--- nyc/views.py
+++ nyc/views.py
@@ -3,11 +3,9 @@ from django.views import View
 
 from nyc.boroughs import boroughs
 
-
 class CityView(View):
     def get(self, request):
         return render(request=request, template_name='city.html', context={'boroughs': boroughs.keys()})
-
 
 class BoroughView(View):
     def get(self, request, borough):
@@ -18,9 +16,22 @@ class BoroughView(View):
         )
 
 
-class ActivityView(View):
-    pass
 
+
+
+
+class ActivityView(View):
+    def get(self, request, borough, activity):
+        return render(
+            request=request,
+            template_name='activity.html',
+            context={'borough': borough, 'activities': boroughs[borough].keys(), 'activity': activity, 'venues':boroughs[borough][activity].keys()},
+        )
 
 class VenueView(View):
-    pass
+    def get(self, request, borough, activity, venue):
+        return render(
+            request=request,
+            template_name='venue.html',
+            context={'borough': borough, 'activities': boroughs[borough].keys(), 'activity': activity, 'venues':boroughs[borough][activity].keys(), 'venue': boroughs[borough][activity][venue]['description'] },
+        )
